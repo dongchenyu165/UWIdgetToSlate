3d2c728a3c23aaae691fefc393e73c131a84981f
dongchenyu165 <dongchenyu165@gmail.com>
2025-02-12 03:50:43 +0900

    build: Add Json and JsonUtilities as dependencies in SlateConverter module

--- Source/SlateConverter/SlateConverter.Build.cs
+++ Source/SlateConverter/SlateConverter.Build.cs
@@ -46,7 +46,9 @@ public class SlateConverter : ModuleRules
 				"EditorStyle",
 				"Blutility",
 				"UMG",
-				"UMGEditor"
+				"UMGEditor",
+				"Json",
+				"JsonUtilities"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
